48879d8031c53541ecc87a451b163e8a7ca434a4
Wissam LEFEVRE <wissam.lefevre@etudiant.univ-lille1.fr>
2016-02-26 11:39:45 +0100

    Modif

--- webserver/socket.c
+++ webserver/socket.c
@@ -5,26 +5,27 @@
 #include "socket.h"
 
 int creer_serveur(int port) {
-
-	int socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
-	if (socket_serveur == -1) {
-    		perror("Socket serveur bug");
-		return -1;
-	}
-
-	struct sockaddr_in saddr;
-	saddr.sin_family = AF_INET;
-	saddr.sin_port = htons(port);
-	saddr.sin_addr.s_addr = INADDR_ANY;
-	if (bind(socket_serveur, (struct sockaddr *) &saddr, sizeof(saddr)) == -1) {
-    		perror(" bind socker_serveur ");
-		return -1;
-	}
-
-	if (listen(socket_serveur, 10) == -1) {
-    		perror("Listen socket_serveur fail");
-		return -1;
-	}
-
-	return socket_serveur;
+  
+  printf("Creation du serveur sur le port %d\n", port);
+  int socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
+  if (socket_serveur == -1) {
+    perror("Socket serveur bug");
+    return -1;
+  }
+  
+  struct sockaddr_in saddr;
+  saddr.sin_family = AF_INET;
+  saddr.sin_port = htons(port);
+  saddr.sin_addr.s_addr = INADDR_ANY;
+  if (bind(socket_serveur, (struct sockaddr *) &saddr, sizeof(saddr)) == -1) {
+    perror("bind socker_serveur");
+    return -1;
+  }
+  printf("Socket rattachee au port %d\n", port);
+  if (listen(socket_serveur, 10) == -1) {
+    perror("Listen socket_serveur fail");
+    return -1;
+  }
+  printf("Listening\n");
+  return socket_serveur;
 }
