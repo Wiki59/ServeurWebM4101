86d35648185e559229f31009a7b1af47f2146e89
Wissam LEFEVRE <wissam.lefevre@etudiant.univ-lille1.fr>
2016-03-04 10:24:43 +0100

    Tout est okay

--- webserver/socket.c
+++ webserver/socket.c
@@ -7,8 +7,6 @@
 #include "socket.h"
 
 int creer_serveur(int port) {
-<<<<<<< HEAD
-
 	int socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
 	if (socket_serveur == -1) {
     		perror("Socket server bug");
@@ -40,29 +38,4 @@ int creer_serveur(int port) {
 
 	close(socket_serveur);
 	return socket_serveur;
-=======
-  
-  printf("Creation du serveur sur le port %d\n", port);
-  int socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
-  if (socket_serveur == -1) {
-    perror("Socket serveur bug");
-    return -1;
-  }
-  
-  struct sockaddr_in saddr;
-  saddr.sin_family = AF_INET;
-  saddr.sin_port = htons(port);
-  saddr.sin_addr.s_addr = INADDR_ANY;
-  if (bind(socket_serveur, (struct sockaddr *) &saddr, sizeof(saddr)) == -1) {
-    perror("bind socker_serveur");
-    return -1;
-  }
-  printf("Socket rattachee au port %d\n", port);
-  if (listen(socket_serveur, 10) == -1) {
-    perror("Listen socket_serveur fail");
-    return -1;
-  }
-  printf("Listening\n");
-  return socket_serveur;
->>>>>>> 39c023c269237adc9ae28dc44d19dec368f72721
 }
